848d2b1e0964afcd2e471a232479e1ee706becce
Gabriel Diniz <gabrieldiniz.contato@gmail.com>
2024-06-22 14:01:08 -0300

    :wrench: chore: Criação do THEME

---
+++ src/theme/colors.ts
@@ -0,0 +1,14 @@
+const colors = {
+    primary: '#FF0044',
+    secondary: '#2ecc71',
+    accent: '#B30D39',
+    background: '#4E4E4E',
+    text: '#FFF',
+    textSecondary: '#FFF',
+    white: '#FFF',
+    black: '#000000',
+    gray: '#CCC8C8',
+    darkGray: '4E4E4E'
+};
+
+export default colors;
