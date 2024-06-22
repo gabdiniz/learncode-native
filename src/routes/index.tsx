3fab84bc8e75959c2e3f4a9c0271bd25e591d17d
Gabriel Diniz <gabrieldiniz.contato@gmail.com>
2024-06-22 19:30:26 -0300

    :wrench: chore: Configuração inicial das rotas

---
+++ src/routes/index.tsx
@@ -0,0 +1,22 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { AppRoutes } from './app.routes';
+import { AuthRoutes } from './auth.routes';
+
+interface Props {
+    isAuth: boolean;
+}
+
+export function Routes({ isAuth }: Props) {
+
+    return (
+        <NavigationContainer>
+            {
+                isAuth
+                ?
+                <AppRoutes />
+                :
+                <AuthRoutes />
+            }
+        </NavigationContainer>
+    )
+}
